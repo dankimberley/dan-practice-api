f96f3eeb51ad90285ecbe603a06e8a29432cff52
Dan Kimberley <danielkimberley@gmail.com>
2024-08-01 18:56:40 +0100

    added earth route

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def welcome(name):
     return 'Welcome ' + name
 
+@app.route('/earth')
+def earth():
+    return 'The circumference of the earth is 40,075km'
+
 if __name__ == '__main__':
     app.run()
